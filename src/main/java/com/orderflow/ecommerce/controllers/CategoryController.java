5718a8070e20881fd1c8c41ba7a108dcad4c5c1d
gio448 <giovannalethicia385@gmail.com>
2026-03-26 19:28:36 -0300

    feat: implementation of Product and Category CRUD and JPA adjustments

---
+++ src/main/java/com/orderflow/ecommerce/controllers/CategoryController.java
@@ -0,0 +1,47 @@
+package com.orderflow.ecommerce.controllers;
+
+import com.orderflow.ecommerce.entities.Category;
+import com.orderflow.ecommerce.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryController {
+
+    @Autowired
+    private CategoryRepository repository;
+
+    @GetMapping
+    public ResponseEntity<List<Category>> findAll() {
+        return ResponseEntity.ok().body(repository.findAll());
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Category> findById(@PathVariable Long id) {
+        return repository.findById(id)
+                .map(obj -> ResponseEntity.ok().body(obj))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Category> insert(@RequestBody Category obj) {
+        return ResponseEntity.ok().body(repository.save(obj));
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        repository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Category> update(@PathVariable Long id, @RequestBody Category obj) {
+        Category entity = repository.findById(id).get();
+        entity.setName(obj.getName());
+        return ResponseEntity.ok().body(repository.save(entity));
+    }
+}
